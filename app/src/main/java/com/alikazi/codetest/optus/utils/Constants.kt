41635ec80263cb189c6e9a924d8cf38cee7764c6
mdalikazi <mdalikazi@gmail.com>
2020-06-19 20:21:21 +1000

    Added layout for photo fragment

--- app/src/main/java/com/alikazi/codetest/optus/utils/Constants.kt
+++ app/src/main/java/com/alikazi/codetest/optus/utils/Constants.kt
@@ -3,7 +3,6 @@ package com.alikazi.codetest.optus.utils
 object Constants {
 
     const val LOG_TAG = "OPTUS"
-    const val DEFAULT_ANIMATION_DURATION = 400L
 
     const val URL_BASE = "https://jsonplaceholder.typicode.com/"
     const val URL_USERS = "users"
