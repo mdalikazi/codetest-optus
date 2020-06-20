82038d27fe977ccee7f03c295d52dcc4a74a8c4b
mdalikazi <mdalikazi@gmail.com>
2020-06-20 20:12:55 +1000

    Added NetworkHelper with Retrofit

---
+++ app/src/main/java/com/alikazi/codetest/optus/network/NetworkHelper.kt
@@ -0,0 +1,35 @@
+package com.alikazi.codetest.optus.network
+
+import com.alikazi.codetest.optus.utils.Constants
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import java.util.concurrent.TimeUnit
+
+object NetworkHelper {
+
+    private val service: Network by lazy {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.URL_BASE)
+            .build()
+
+        retrofit.create(Network::class.java)
+    }
+
+    interface Network {
+        @GET(Constants.URL_USERS)
+        fun getUsers(): String
+
+        @GET(Constants.URL_PHOTOS)
+        fun getPhotos(): String
+    }
+
+}
