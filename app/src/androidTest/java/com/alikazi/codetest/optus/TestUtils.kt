a7ce2f292a384256923268b97e764fe074e491a0
mdalikazi <mdalikazi@gmail.com>
2020-06-23 19:35:15 +1000

    Added a FragmentsOpenTest UI test to tap on 1st item in recycler views for users and album and check if they open corresponing fragments.

---
+++ app/src/androidTest/java/com/alikazi/codetest/optus/TestUtils.kt
@@ -0,0 +1,25 @@
+package com.alikazi.codetest.optus
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+
+object TestUtils {
+
+    fun <VH : RecyclerView.ViewHolder> clickOnFirstItemInRecyclerView(recyclerViewId: Int) {
+        Espresso.onView(ViewMatchers.withId(recyclerViewId))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<VH>
+                        (0, ViewActions.click()))
+    }
+
+    fun isRecyclerViewNotEmpty(recyclerView: RecyclerView): Boolean {
+        return recyclerView.adapter != null && recyclerView.adapter?.itemCount != 0
+    }
+
+}
